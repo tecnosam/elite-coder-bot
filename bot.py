b154167f7cf0348fe43326232228be682023f763
Abolo Samuel <ikabolo59@gmail.com>
2021-05-07 02:53:07 +0100

    initial release of bot

---
+++ bot.py
@@ -0,0 +1,43 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content
+    if ('asimo:' == msg[:6]):
+        await message.channel.send( 
+                "Hello. the admin is still working on my friendly AI he'll message you on updates and even more cool commands. Thanks"
+        )
+
+
+client.run(TOKEN)
